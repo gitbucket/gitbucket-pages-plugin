4cf1d5e2d858197a719d0767ce7aff0726e867aa
tsu <tsu@yaroot.net>
2016-06-06 21:15:13 +0800

    read plugin version from jar manifest, fixes #4

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -4,12 +4,30 @@ import gitbucket.plugin.pages.PagesController
 import io.github.gitbucket.solidbase.model.Version
 
 class Plugin extends gitbucket.core.plugin.Plugin {
+  import gitbucket.plugin.pages.util.Version
   override val pluginId = "pages"
   override val pluginName = "Pages Plugin"
-  override val description = "Provides Pages feature on GitBucket"
-  override val versions = List(new Version("0.0.0"))
+  override val description = "Project pages for gitbucket"
+  override val versions = List(new Version(Version.version.getOrElse("unknown")))
 
   override val controllers: Seq[(String, ControllerBase)] = Seq(
     "/*" -> new PagesController
   )
+}
+
+package gitbucket.plugin.pages.util {
+  import scala.collection.JavaConverters._
+  import scala.util.Try
+  import java.util.jar.{ Manifest => JarManifest }
+
+  object Version {
+    val version = Try(getVersionFromManifests()).toOption.flatten
+    def getVersionFromManifests() = {
+      val resources = getClass.getClassLoader.getResources("META-INF/MANIFEST.MF").asScala.toList
+      resources
+        .flatMap(res => Try(new JarManifest(res.openStream())).toOption.toList)
+        .find(manifest => Option(manifest.getMainAttributes.getValue("Implementation-Title")).contains("pages-plugin"))
+        .flatMap(manifest => Option(manifest.getMainAttributes.getValue("Implementation-Version")))
+    }
+  }
 }
