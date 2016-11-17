f4ba02590c1dcc9673937033b5a00661711ac385
tsu <tsu@yaroot.net>
2016-11-17 16:39:43 +0800

    fix version migration

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -1,13 +1,21 @@
 import gitbucket.core.controller.ControllerBase
 import gitbucket.plugin.pages.PagesController
-import gitbucket.plugin.pages.util.VersionUtil
 import io.github.gitbucket.solidbase.model.Version
 
 class Plugin extends gitbucket.core.plugin.Plugin {
   override val pluginId = "pages"
   override val pluginName = "Pages Plugin"
   override val description = "Project pages for gitbucket"
-  override val versions = List(new Version(VersionUtil.version.getOrElse("unknown")))
+  override val versions = List(
+    new Version("0.1"),
+    new Version("0.2"),
+    new Version("0.3"),
+    new Version("0.4"),
+    new Version("0.5"),
+    new Version("0.6"),
+    new Version("0.7"),
+    new Version("0.8")
+  )
 
   override val controllers: Seq[(String, ControllerBase)] = Seq(
     "/*" -> new PagesController
