42d82d99efd0e02bc99c4424e4a2b7b25956a3ea
Naoki Takezoe <takezoe@gmail.com>
2018-06-10 16:54:57 +0900

    Bump to 1.7.1

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -31,7 +31,8 @@ class Plugin extends gitbucket.core.plugin.Plugin {
     new Version("1.4.0"),
     new Version("1.5.0"),
     new Version("1.6.0"),
-    new Version("1.7.0")
+    new Version("1.7.0"),
+    new Version("1.7.1")
   )
 
   override val controllers: Seq[(String, ControllerBase)] = Seq("/*" -> new PagesController)
