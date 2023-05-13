4a22f7ed73ad1d9c3258b54ff21ca6053fb95245
xuwei-k <6b656e6a69@gmail.com>
2023-05-13 12:42:42 +0900

    update scalafmt settings

--- src/main/scala/gitbucket/plugin/service/PagesService.scala
+++ src/main/scala/gitbucket/plugin/service/PagesService.scala
@@ -21,8 +21,7 @@ trait PagesService {
       .map(t => t.source)
       .update(source)
 
-  def renameRepository(userName: String, oldRepositoryName: String, newRepositoryName: String)(
-    implicit
+  def renameRepository(userName: String, oldRepositoryName: String, newRepositoryName: String)(implicit
     s: Session
   ): Unit =
     Pages
