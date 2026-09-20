44c83d68855a19c5e12573010ddcd015cb949680
takezoe <takezoe@gmail.com>
2026-09-20 14:57:44 +0900

    Release 1.11.0

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -35,7 +35,8 @@ class Plugin extends gitbucket.core.plugin.Plugin {
     new Version("1.7.1"),
     new Version("1.8.0"),
     new Version("1.9.0"),
-    new Version("1.10.0")
+    new Version("1.10.0"),
+    new Version("1.11.0")
   )
 
   override val controllers: Seq[(String, ControllerBase)] = Seq("/*" -> new PagesController)
